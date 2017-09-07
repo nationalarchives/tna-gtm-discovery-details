a2972282695e969c3b101e088c1c3c358f367177
Punal Chotrani <punalsc@gmail.com>
2017-09-07 15:04:56 +0100

    setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,12 @@ module.exports = function (grunt) {
         uglify: {
             target: {
                 files: {
-                    'js/scripts.min.js': 'js/scripts.js'
+                    'js/script.min.js': 'js/script.js'
                 }
             }
         },
         eslint: {
-            target: "src/scripts.js",
+            target: "src/script.js",
             options: {
                 configFile: 'conf/eslint.json'
             }
@@ -22,56 +22,27 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    "js/scripts.js": "src/scripts.js"
+                    "js/script.js": "src/script.js"
                 }
             }
         },
-        qunit: {
-            all: ['js/tests/**/*.html']
-        },
         watch: {
             scripts: {
-                files: ['src/scripts.js', 'tests/test.js'],
+                files: ['src/script.js'],
                 tasks: ['default'],
                 options: {
                     spawn: false
                 }
-            },
-            css: {
-                files: 'css/sass/*.scss',
-                tasks: ['sass', 'cssmin']
-            }
-        },
-        sass: {
-            build: {
-                files: [{
-                    src: ['css/sass/tnaWifi.scss'],
-                    dest: 'css/tnaWifi.css'
-                }]
-            }
-        },
-        cssmin: {
-            target: {
-                files: [{
-                    expand: true,
-                    cwd: 'css/',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'css/',
-                    ext: '.min.css'
-                }]
             }
         }
     });
 
     //Load the plugins
     grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     //Default Grunt tasks
-    grunt.registerTask('default', ['eslint', 'babel', 'sass', 'cssmin', 'uglify', 'watch']);
+    grunt.registerTask('default', ['eslint', 'babel', 'uglify', 'watch']);
 };
