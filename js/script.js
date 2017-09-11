3f1207babd0d36399d4cc58b4b61ab7bfe0fc9a2
Punal Chotrani <punalsc@gmail.com>
2017-09-11 15:36:07 +0100

    on loading the dataLayer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 'use strict';
 
-(function ($) {
-    var $meta = $('meta[name=WT\\.cg_n]').attr('content');
-    console.log($meta);
-})(jQuery);
+jQuery(window).on('load', function () {
+
+    var $dataContent = $('meta[name=WT\\.cg_n]').attr('content'),
+        $metaName = $('meta[name=WT\\.cg_n]').attr('name');
+
+    if ($metaName === 'WT.cg_n') {
+        $metaName = 'DiscoveryContentGroup';
+    }
+
+    dataLayer.push({
+        'event': 'Discovery',
+        'eventCategory': 'Details Page',
+        'eventAction': $metaName,
+        'eventLabel': $dataContent
+    });
+});
 //# sourceMappingURL=script.js.map
--- src/script.js
+++ src/script.js
@@ -1,4 +1,14 @@
-(function( $ ) {
-    let $meta = $('meta[name=WT\\.cg_n]').attr('content');
-    console.log($meta);
-})( jQuery );
+jQuery(window).on('load', () => {
+
+    let $dataContent = $('meta[name=WT\\.cg_n]').attr('content'),
+        $metaName = $('meta[name=WT\\.cg_n]').attr('name');
+
+    if ( $metaName === 'WT.cg_n') { $metaName = 'DiscoveryContentGroup'; }
+
+    dataLayer.push({
+        'event'         : 'Discovery',
+        'eventCategory' : 'Details Page',
+        'eventAction'   : $metaName,
+        'eventLabel'    : $dataContent
+    });
+});
