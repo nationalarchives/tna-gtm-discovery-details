d0fdb35fe58d405f5eb0520312330220208e4871
Punal Chotrani <punalsc@gmail.com>
2017-09-14 15:21:48 +0100

    updated grunt setup

--- js/gtmDiscoveryDataAttribute.js
+++ js/gtmDiscoveryDataAttribute.js
@@ -1,2 +1,24 @@
-"use strict";
+'use strict';
+
+/**
+ * ------------  THE NATIONAL ARCHIVES  -----------------
+ * JS - getting the data attributes in Discovery Details page for Google Tag Manager
+ * Developer: Punal Chotrani
+ **/
+
+$('.order-option-wrapper a').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(undefined),
+        $discovery = 'Discovery',
+        $eventAction = 'Delivery option chosen',
+        $eventLabel = $this.data('webtrendsCall');
+
+    /* Pushing stuff to Google's Data Layer */
+    dataLayer.push({
+        'event': $discovery,
+        'eventCategory': $discovery,
+        'eventAction': $eventAction,
+        'eventLabel': $eventLabel
+    });
+});
 //# sourceMappingURL=gtmDiscoveryDataAttribute.js.map
