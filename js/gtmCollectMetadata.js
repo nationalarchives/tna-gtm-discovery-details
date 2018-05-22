d132ac60585e40f6d691b500a0bec8e068617f31
Gwyn Jones <m@gwnjns.com>
2018-05-22 11:11:49 +0100

    Fix '$ is undefined' bug
    This bug is evident only when GTM reliant features are deployed to
    TEST. The fix is really simple: placing code that relies upon jQuery
    within a condition that tests it is not 'undefined'.
    
    This approach has been discussed with Helen to ensure it works from
    the analytics/tracking perspective.

--- js/gtmCollectMetadata.js
+++ js/gtmCollectMetadata.js
@@ -7,21 +7,24 @@
  **/
 $(window).on('load', function () {
 
-    var $metaDataContent = $('meta[name=WT\\.cg_n]').attr('content'),
-        $metaDataName = $('meta[name=WT\\.cg_n]').attr('name'),
-        $discovery = 'Discovery';
+    if (typeof $ !== "undefined") {
 
-    if ($metaDataName === 'WT.cg_n') {
-        //$metaDataName = 'DiscoveryContentGroup'; (To be used later with other meta tags)
+        var $metaDataContent = $('meta[name=WT\\.cg_n]').attr('content'),
+            $metaDataName = $('meta[name=WT\\.cg_n]').attr('name'),
+            $discovery = 'Discovery';
 
-        /* Pushing stuff to Google's Data Layer */
-        dataLayer.push({
-            // 'event'         : $discovery, (To be used later with other meta tags)
-            // 'eventCategory' : $discovery, (To be used later with other meta tags)
-            // 'eventAction'   : $metaDataName, (To be used later with other meta tags)
-            // 'eventLabel'    : $metaDataContent (To be used later with other meta tags)
-            'ContentGroup': $metaDataContent
-        });
+        if ($metaDataName === 'WT.cg_n') {
+            //$metaDataName = 'DiscoveryContentGroup'; (To be used later with other meta tags)
+
+            /* Pushing stuff to Google's Data Layer */
+            dataLayer.push({
+                // 'event'         : $discovery, (To be used later with other meta tags)
+                // 'eventCategory' : $discovery, (To be used later with other meta tags)
+                // 'eventAction'   : $metaDataName, (To be used later with other meta tags)
+                // 'eventLabel'    : $metaDataContent (To be used later with other meta tags)
+                'ContentGroup': $metaDataContent
+            });
+        }
     }
 });
 //# sourceMappingURL=gtmCollectMetadata.js.map
